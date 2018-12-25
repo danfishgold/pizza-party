0e0fff9be3524a0096dc67d699cf5894742fd695
Dan Fishgold <d.fishgold@gmail.com>
2018-12-25 17:36:30 +0200

    Format files

--- client/client.js
+++ client/client.js
@@ -1,65 +1,63 @@
-const app = Elm.Main.init()
+const app = Elm.Main.init();
 
-const socket = io.connect('/')
-
+const socket = io.connect("/");
 
 // OUTGOING
 
 app.ports.createRoom.subscribe(data => {
-  socket.emit('create-room', data)
-})
+  socket.emit("create-room", data);
+});
 
 app.ports.joinRoom.subscribe(data => {
-  socket.emit('join-room', data)
-})
+  socket.emit("join-room", data);
+});
 
 app.ports.sendTriplet.subscribe(data => {
-  socket.emit('triplet', data)
-})
+  socket.emit("triplet", data);
+});
 
 app.ports.requestBaseToppingList.subscribe(data => {
-  socket.emit('request-base-topping-list', data)
-})
+  socket.emit("request-base-topping-list", data);
+});
 
 app.ports.sendBaseToppingListOrError.subscribe(data => {
-  socket.emit('base-topping-list', data)
-})
+  socket.emit("base-topping-list", data);
+});
 
 app.ports.sendKickGuest.subscribe(data => {
-  socket.emit('kick-guest', data)
-})
-
+  socket.emit("kick-guest", data);
+});
 
 // INCOMING
 
-socket.on('create-room-response', data => {
-  app.ports.createRoomResponse.send(data)
-})
+socket.on("create-room-response", data => {
+  app.ports.createRoomResponse.send(data);
+});
 
-socket.on('join-room-response', data => {
-  app.ports.joinRoomResponse.send(data)
-})
+socket.on("join-room-response", data => {
+  app.ports.joinRoomResponse.send(data);
+});
 
-socket.on('triplet', data => {
-  app.ports.receiveTriplet.send(data)
-})
+socket.on("triplet", data => {
+  app.ports.receiveTriplet.send(data);
+});
 
-socket.on('request-base-topping-list', data => {
-  app.ports.receiveBaseToppingListRequest.send(data)
-})
+socket.on("request-base-topping-list", data => {
+  app.ports.receiveBaseToppingListRequest.send(data);
+});
 
-socket.on('base-topping-list', data => {
-  app.ports.receiveBaseToppingList.send(data)
-})
+socket.on("base-topping-list", data => {
+  app.ports.receiveBaseToppingList.send(data);
+});
 
-socket.on('guest-left', data => {
-  app.ports.receiveGuestLeft.send(data)
-})
+socket.on("guest-left", data => {
+  app.ports.receiveGuestLeft.send(data);
+});
 
-socket.on('host-left', data => {
-  app.ports.receiveHostLeft.send(data)
-})
+socket.on("host-left", data => {
+  app.ports.receiveHostLeft.send(data);
+});
 
-socket.on('kick-guest', data => {
-  app.ports.receiveKickGuest.send(data)
-})
+socket.on("kick-guest", data => {
+  app.ports.receiveKickGuest.send(data);
+});
--- server.js
+++ server.js
@@ -1,60 +1,61 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-const db = low(new FileSync('.data/db.json'))
+const db = low(new FileSync(".data/db.json"));
 
-db.set('sessions', [])
-  .write()
+db.set("sessions", []).write();
 
+app.use(express.static("client"));
 
-app.use(express.static('client'))
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/client/index.html");
+});
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/client/index.html')
-})
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
-
+  console.log(`Listening on port ${port}`);
+});
 
 function alreadyHost(socket) {
-  const sessionWithSocketAsHost = db.get('sessions')
+  const sessionWithSocketAsHost = db
+    .get("sessions")
     .find({ hostId: socket.id })
-    .value()
+    .value();
 
-  return sessionWithSocketAsHost != undefined
+  return sessionWithSocketAsHost != undefined;
 }
 
 function roomIdExists(roomId) {
-  const sessionWithRoomId = db.get('sessions')
+  const sessionWithRoomId = db
+    .get("sessions")
     .find({ roomId })
-    .value()
-  
-    return sessionWithRoomId != undefined
+    .value();
+
+  return sessionWithRoomId != undefined;
 }
 
 function hostOfRoom(roomId) {
-  const hostId = db.get('sessions')
+  const hostId = db
+    .get("sessions")
     .find({ roomId })
-    .value()
-    .hostId
+    .value().hostId;
 
-  return hostId
+  return hostId;
 }
 
 function createRandomRoomWithHost(socket) {
-  const roomId = Math.random().toString().substring(2, 8)
-  db.get('sessions')
+  const roomId = Math.random()
+    .toString()
+    .substring(2, 8);
+  db.get("sessions")
     .push({ roomId, hostId: socket.id })
-    .write()
+    .write();
 
-  return roomId
+  return roomId;
 }
 
 function joinRoomAndAddHandlers(socket, roomId) {
@@ -62,63 +63,59 @@ function joinRoomAndAddHandlers(socket, roomId) {
     id: roomId,
     name: `room-${roomId}`,
     hostId: hostOfRoom(roomId)
-  }
-  socket.join(room.name)
+  };
+  socket.join(room.name);
 
-  socket.on('request-base-topping-list', data => {
-    room.user = data
-    socket.to(room.hostId).emit('request-base-topping-list', data)
-  })
+  socket.on("request-base-topping-list", data => {
+    room.user = data;
+    socket.to(room.hostId).emit("request-base-topping-list", data);
+  });
 
-  socket.on('base-topping-list', data => {
-    socket.to(room.name).emit('base-topping-list', data)
-  })
+  socket.on("base-topping-list", data => {
+    socket.to(room.name).emit("base-topping-list", data);
+  });
 
-  socket.on('triplet', data => {
-    socket.to(room.name).emit('triplet', data)
-  })
+  socket.on("triplet", data => {
+    socket.to(room.name).emit("triplet", data);
+  });
 
-  socket.on('kick-guest', data => {
-    socket.to(room.name).emit('kick-guest', data)
-  })
+  socket.on("kick-guest", data => {
+    socket.to(room.name).emit("kick-guest", data);
+  });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     if (room.hostId == socket.id) {
-      socket.to(room.name).emit('host-left')
-      db.get('sessions')
+      socket.to(room.name).emit("host-left");
+      db.get("sessions")
         .remove({ roomId: room.id })
-        .write()
+        .write();
     } else {
-      socket.to(room.hostId).emit('guest-left', room.user)
+      socket.to(room.hostId).emit("guest-left", room.user);
     }
-    
-  })
+  });
 }
 
-
-io.on('connection', socket => {
-
-  socket.on('create-room', () => {
+io.on("connection", socket => {
+  socket.on("create-room", () => {
     if (!alreadyHost(socket)) {
-      const roomId = createRandomRoomWithHost(socket)
-      joinRoomAndAddHandlers(socket, roomId)
-      socket.emit('create-room-response', { ok: { roomId } })
+      const roomId = createRandomRoomWithHost(socket);
+      joinRoomAndAddHandlers(socket, roomId);
+      socket.emit("create-room-response", { ok: { roomId } });
     } else {
-      socket.emit('create-room-response', 
-        { error: "The user is already a host" }
-      )
+      socket.emit("create-room-response", {
+        error: "The user is already a host"
+      });
     }
-  })
+  });
 
-  socket.on('join-room', roomId => {
+  socket.on("join-room", roomId => {
     if (roomIdExists(roomId)) {
-      joinRoomAndAddHandlers(socket, roomId)
-      socket.emit('join-room-response', { ok: roomId })
+      joinRoomAndAddHandlers(socket, roomId);
+      socket.emit("join-room-response", { ok: roomId });
     } else {
-      socket.emit('join-room-response', 
-        { error: "There's no room with this id" }
-      )
+      socket.emit("join-room-response", {
+        error: "There's no room with this id"
+      });
     }
-  })
-})
-  
+  });
+});
