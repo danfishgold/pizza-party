dd7e33057a6a412dee3a5ca16272a46f1c966475
Dan Fishgold <d.fishgold@gmail.com>
2018-12-27 21:05:27 +0200

    Make pie-chart-like diagrams

--- src/Diagram.elm
+++ src/Diagram.elm
@@ -5,7 +5,7 @@ import Config
 import Count
 import Division
 import Svg exposing (Svg, g, path, svg, text, text_)
-import Svg.Attributes exposing (d, fill, height, stroke, textAnchor, transform, width, x, y)
+import Svg.Attributes exposing (d, fill, height, stroke, textAnchor, textDecoration, textLength, transform, width, x, y)
 import Topping exposing (Topping)
 
 
@@ -49,11 +49,17 @@ pies radius config toppingCount =
 pie : Float -> Int -> List Topping.Pair -> Svg msg
 pie radius slicesPerPie pieCount =
     let
+        wd =
+            2 * radius + 300
+
+        ht =
+            2 * radius + 80
+
         translation =
             "translate("
-                ++ String.fromFloat (radius + 1)
+                ++ String.fromFloat (wd / 2)
                 ++ ","
-                ++ String.fromFloat (radius + 1)
+                ++ String.fromFloat (ht / 2)
                 ++ ")"
     in
     pieCount
@@ -63,8 +69,8 @@ pie radius slicesPerPie pieCount =
         |> g [ transform translation ]
         |> List.singleton
         |> svg
-            [ width <| String.fromFloat <| radius * 2 + 2
-            , height <| String.fromFloat <| radius * 2 + 2
+            [ width <| String.fromFloat <| wd
+            , height <| String.fromFloat <| ht
             , Svg.Attributes.style "margin: 10px"
             ]
 
@@ -88,8 +94,8 @@ sliceGroupsFromPairs slicesPerPie pieCount =
 
                 ( topping, count ) :: tl ->
                     { topping = topping
-                    , startAngle = offset + toFloat startIndex * sliceRange
-                    , endAngle = offset + toFloat (startIndex + count) * sliceRange
+                    , startAngle = mod2pi <| offset + toFloat startIndex * sliceRange
+                    , endAngle = mod2pi <| offset + toFloat (startIndex + count) * sliceRange
                     }
                         :: helper (startIndex + count) tl
     in
@@ -121,7 +127,7 @@ arc r startAngle endAngle color =
                 1
 
         pathComponents =
-            if startAngle == endAngle - 2 * pi then
+            if startAngle == endAngle || startAngle == endAngle - 2 * pi then
                 [ pathCommand "M" [ start.x, start.y ]
                 , pathCommand "A" [ r, r, 0, arcSweep, 1, end.x, end.y ]
                 ]
@@ -143,45 +149,73 @@ arc r startAngle endAngle color =
 
 arcTitle : Float -> SliceGroup -> Color -> Svg msg
 arcTitle r { startAngle, endAngle, topping } color =
-    let
-        center =
-            arcCenter r startAngle endAngle
-    in
-    text_
-        [ x <| String.fromFloat center.x
-        , y <| String.fromFloat center.y
-        , textAnchor "middle"
-        , fill <| Color.toCssString color
-        ]
-        [ text <| Topping.toString topping ]
-
-
-arcCenter : Float -> Float -> Float -> { x : Float, y : Float }
-arcCenter r startAngle endAngle =
-    if endAngle <= startAngle then
-        arcCenter r startAngle (endAngle + 2 * pi)
+    if abs (endAngle - startAngle) == 2 * pi || startAngle == endAngle then
+        text_
+            [ x "0"
+            , y "0"
+            , textAnchor "middle"
+            ]
+            [ text <| Topping.toString topping ]
 
     else
         let
-            alpha =
-                endAngle - startAngle
+            angle =
+                middleAngle startAngle endAngle
 
-            rad =
-                if alpha == 2 * pi then
-                    0
+            pt1 =
+                polarToCartesian (2 / 3 * r) angle
 
-                else
-                    2 * r / 3 * (1 - alpha / (8 * pi))
+            pt2 =
+                polarToCartesian (4 / 3 * r) angle
 
-            theta =
-                startAngle + alpha / 2
+            isOnRight =
+                -pi / 2 <= angle && angle <= pi / 2
         in
-        polarToCartesian rad theta
+        g []
+            [ path
+                [ d <|
+                    String.join " " <|
+                        [ pathCommand "M" [ pt1.x, pt1.y ]
+                        , pathCommand "L" [ pt2.x, pt2.y ]
+                        ]
+                , stroke "black"
+                , fill "none"
+                ]
+                []
+            , text_
+                [ x <| String.fromFloat pt2.x
+                , y <| String.fromFloat (pt2.y - 1.5)
+                , textAnchor <|
+                    if isOnRight then
+                        "start"
+
+                    else
+                        "end"
+                , textDecoration "underline"
+                , fill <| Color.toCssString color
+                ]
+                [ text <| Topping.toString topping ]
+            ]
 
 
-pathCommand : String -> List Float -> String
-pathCommand command values =
-    command :: List.map String.fromFloat values |> String.join " "
+middleAngle : Float -> Float -> Float
+middleAngle startAngle endAngle =
+    if startAngle == endAngle then
+        startAngle
+
+    else if endAngle <= startAngle then
+        middleAngle startAngle (endAngle + 2 * pi)
+
+    else
+        let
+            middle =
+                startAngle + (endAngle - startAngle) / 2
+        in
+        if middle == 0 || middle == pi then
+            middleAngle (min startAngle endAngle) middle
+
+        else
+            middle
 
 
 polarToCartesian : Float -> Float -> { x : Float, y : Float }
@@ -189,3 +223,19 @@ polarToCartesian r angle =
     { x = r * cos angle
     , y = r * sin angle
     }
+
+
+pathCommand : String -> List Float -> String
+pathCommand command values =
+    command :: List.map String.fromFloat values |> String.join " "
+
+
+mod2pi angle =
+    if angle >= 3 / 2 * pi then
+        mod2pi (angle - 2 * pi)
+
+    else if angle < -pi / 2 then
+        mod2pi (angle + 2 * pi)
+
+    else
+        angle
